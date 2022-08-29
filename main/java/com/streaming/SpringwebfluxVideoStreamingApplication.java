1b3bf2f6f88dd4903ea4ea5d639753e95205b363
mohdanas2411 <anas06132@gmail.com>
2022-08-29 14:08:48 +0530

    streaming changes

--- main/java/com/streaming/SpringwebfluxVideoStreamingApplication.java
+++ main/java/com/streaming/SpringwebfluxVideoStreamingApplication.java
@@ -1,4 +1,4 @@
-package com.javatechie.streaming;
+package com.streaming;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
--- test/java/com/streaming/SpringwebfluxVideoStreamingApplicationTests.java
+++ test/java/com/streaming/SpringwebfluxVideoStreamingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javatechie.streaming;
+package com.streaming;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
